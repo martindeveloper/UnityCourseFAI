2f16ef4b5ffafbd16dd4c12808de1b109d7418ff
Martin Pernica <martin.developer@hotmail.com>
2017-04-26 00:34:02 +0200

    Added source code from first session
    Without any further modification

---
+++ Session1/Assets/LevelInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelInfo : MonoBehaviour
+{
+    public void Awake()
+    {
+        if(SceneManager.sceneCount == 1)
+        {
+            SceneManager.LoadScene("Scenes/BaseScene", LoadSceneMode.Additive);
+        }
+    }
+}
---
+++ Session1/Assets/Scripts/MainMenuCanvas.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuCanvas : MonoBehaviour
+{
+    public UnityEngine.UI.Button StartButton;
+
+    public void Start()
+    {
+        StartButton.onClick.AddListener(OnStartButtonClicked);
+    }
+
+    private void OnStartButtonClicked()
+    {
+        SceneManager.LoadScene("Scenes/Levels/Level1", LoadSceneMode.Additive);
+
+        Debug.Log("Start button cliked!");
+    }
+}
