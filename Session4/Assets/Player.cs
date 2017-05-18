685edb93fc3f3a96bc4aa708b6f7df82264f522a
Martin Pernica <martin.developer@hotmail.com>
2017-05-18 20:17:13 +0200

    Added Session 4

---
+++ Session4/Assets/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tags
+{
+    public const string TreeEntity = "TreeEntity";
+    public const string AppleEntity = "AppleEntity";
+}
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody ColliderRigidbody;
+    public Joint HandJoint;
+
+    public void Start()
+    {
+        HandJoint = GetComponent<Joint>();
+        HandJoint.enablePreprocessing = false;
+    }
+
+    public void FixedUpdate()
+    {
+        if(Input.GetKeyUp(KeyCode.E))
+        {
+            RaycastHit hit;
+
+            bool isHit = Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 5.0f);
+
+            if(isHit)
+            {
+                //Debug.DrawLine(Camera.main.transform.position, hit.point, Color.red, 5.0f);
+
+                if (hit.collider.CompareTag(Tags.TreeEntity))
+                {
+                    TreeEntity tree = hit.collider.GetComponent<TreeEntity>();
+
+                    Debug.LogFormat("Maximum Apples {0}", tree.GetMaximumApples());
+
+                    bool areApplesSpawned = tree.SpawnApples(5);
+
+                    Debug.LogFormat("Spawning apples {0}", areApplesSpawned);
+                }
+
+                if(hit.collider.CompareTag(Tags.AppleEntity))
+                {
+                    //hit.collider.GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * 1000);
+                    Rigidbody appleRigidbody = hit.collider.GetComponent<Rigidbody>();
+
+                    HandJoint.connectedBody = appleRigidbody;
+                }
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.LogFormat("Entered to trigger named {0}", other.name);
+    }
+}
---
+++ Session4/Assets/TreeEntity.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeEntity : MonoBehaviour
+{
+    public GameObject ApplePrefab;
+
+    [Range(0, 10)]
+    public int TimeOut = 5;
+
+    private bool CanSpawnApples = true;
+
+    public int GetMaximumApples()
+    {
+        return Random.Range(10, 20);
+    }
+
+    public bool SpawnApples(int number)
+    {
+        if(CanSpawnApples)
+        {
+            Collider collider = GetComponent<Collider>();
+
+            for(int i = 0; i < number; i++)
+            {
+                Instantiate(ApplePrefab, MakeRandomPositionInBounds(collider.bounds), Quaternion.identity);
+            }
+
+            CanSpawnApples = false;
+
+            StartCoroutine(ResetSpawning());
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public Vector3 MakeRandomPositionInBounds(Bounds bounds)
+    {
+        Vector3 position = new Vector3(
+            Random.Range(bounds.extents.x, -bounds.extents.x),
+            Random.Range(bounds.extents.y, -bounds.extents.y),
+            Random.Range(bounds.extents.z, -bounds.extents.z)
+            );
+
+        return bounds.center - position;
+    }
+
+    private IEnumerator ResetSpawning()
+    {
+        yield return new WaitForSeconds(TimeOut);
+
+        CanSpawnApples = true;
+    }
+}
